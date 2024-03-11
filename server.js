e970c70ff7a956df98d0a88d8fc28a26c578153e
Ahmed Elnogoumi <ahmedelnogoumi@gmail.com>
2024-03-11 14:13:17 -0700

    finished setup

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose')
+const Book = require('./model/book.js')
 
 const DATABASE_URL = process.env.DATABASE_URL;
 
@@ -12,10 +13,9 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/test', (request, response) => {
-
-  response.send('test request received')
-
+app.get('/books', async (request, response) => {
+  let documents = await Book.find(); // should return every model instance in the DB
+  response.send(documents);
 })
 
 mongoose.connect(DATABASE_URL)
