9c1b19745f2bb028c62c34c0cde1e9176ead85bc
Brock Britton <brockbritton@outlook.com>
2024-03-12 15:23:24 -0700

    some changes

--- server.js
+++ server.js
@@ -10,12 +10,24 @@ const DATABASE_URL = process.env.DATABASE_URL;
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
 
 app.get('/books', async (request, response) => {
   let documents = await Book.find(); // should return every model instance in the DB
   response.send(documents);
+});
+
+app.post('/books', async (request, response) => {
+  try {
+    let json = request.body;
+    console.log('HERE ARE THE BOOK VALUES', json);
+    let newBooks = await Book.create(json);
+    response.send(newBooks);
+  } catch (error) {
+    response.status(500)
+  }
 })
 
 mongoose.connect(DATABASE_URL)
